d20db16cee39665138778854c5060d0252e4aed3
SwordMaster <sm@swordmaster.tech>
2022-06-01 20:03:37 +0700

    Updated code

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,69 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int incomeTotal = 0;
+  int expenseTotal = 0;
+  int diff = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text("Dejariana"),
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0.0,
       ),
 
-      body: Center(
-        child:RaisedButton.icon(
-          onPressed: (){},
-          icon: Icon(
-            Icons.mail
-          ),
-          label: Text(
-          "Test 123",
-          style: TextStyle(
-            fontSize: 25.0,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
+      body:Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+            'TOTAL INCOME',
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,
+                  )
+              ),
+            SizedBox(height: 10.0),
+            Text(
+                '$incomeTotal',
+                style: TextStyle(
+                    color: Colors.lightGreen,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+            )
+        ),
+        SizedBox(height: 40.0),
+            Text(
+                'TOTAL EXPENSE',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )
             ),
-          ),
-          color: Colors.amber,
+            SizedBox(height: 10.0),
+            Text(
+                '$expenseTotal',
+                style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
