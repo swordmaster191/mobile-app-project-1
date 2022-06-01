4b890a04a880109f7f1701f67f2f728ab7036679
SwordMaster <sm@swordmaster.tech>
2022-06-01 17:33:01 +0700

    Add stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,37 @@ import 'package:flutter/material.dart';
 //https://www.youtube.com/watch?v=km2P_KQJyO0&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ&index=6
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Dejariana"),
+        backgroundColor: Colors.green,
       ),
 
       body: Center(
-        child: Text("Test 123"),
+        child: Text(
+          "Test 123",
+          style: TextStyle(
+            fontSize: 25.0,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child:
-          Text("+"),
+        Text("+"),
+        backgroundColor: Colors.blue,
       ),
-    ),
-  ));
+    );
+  }
 }
